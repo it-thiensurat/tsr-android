16d8a3eafc413e7b0a105515a782946594d848c9
thiensurat <it.department@thiensurat.co.th>
2019-08-15 14:44:43 +0700

    Commit 14/07/2019 Add new folder retrofit

---
+++ tsr/src/th/co/thiensurat/retrofit/api/Service.java
@@ -0,0 +1,30 @@
+package th.co.thiensurat.retrofit.api;
+
+
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by frank on 12/16/16.
+ */
+
+public interface Service {
+
+
+    @GET("/api/api-createpaymentdaily.php")
+    Call<Object> data(@Query("Empid") String data);
+
+
+
+
+
+
+
+}
+
+
