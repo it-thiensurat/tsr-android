a7b38a44a346638f318d5c594389bb3f779de009
thiensurat <it.department@thiensurat.co.th>
2019-11-08 14:10:08 +0700

    Commit 08/11/2019 14.00 Add new file get data

---
+++ tsr/src/th/co/thiensurat/data/Get_data_online.java
@@ -0,0 +1,583 @@
+package th.co.thiensurat.data;
+
+public class Get_data_online {
+    String ReceiptCode;
+
+    String CONTNO = "";
+    String CustomerName = "";
+    String IDCard = "";
+    String AddressInstall = "";
+    String ProductName = "";
+    String MODEL = "";
+    String ProductSerialNumber = "";
+    String MaxPaymentPeriodNumber = "";
+    String TotalPayment = "";
+    String TotalPaymentText = "";
+    String PeriodTotal = "";
+    String PeriodTotalPrice = "";
+    String EFFDATE = "";
+    String DatePayment = "";
+    String ChanelName = "";
+    String TeamName = "";
+    String SalePaymentPeriod_SalePaymentPeriodID = "";
+    String SalePaymentPeriod_RefNo = "";
+    String SalePaymentPeriod_PaymentPeriodNumber = "";
+    String SalePaymentPeriod_PaymentAmount = "";
+    String SalePaymentPeriod_Discount = "";
+    String SalePaymentPeriod_NetAmount = "";
+    String SalePaymentPeriod_PaymentComplete = "";
+    String SalePaymentPeriod_TripID = "";
+    String SalePaymentPeriod_CreateBy = "";
+    String SalePaymentPeriod_LastUpdateBy = "";
+    String SalePaymentPeriod_CloseAccountDiscountAmount = "";
+    String SalePaymentPeriod_CreateDate = "";
+    String SalePaymentPeriod_PaymentDueDate = "";
+    String SalePaymentPeriod_PaymentAppointmentDate = "";
+    String SalePaymentPeriod_LastUpdateDate = "";
+    String SalePaymentPeriod_SyncedDate = "";
+    String DatePayment1 = "";
+    String paymentID = "";
+    String PaymentID = "";
+    String OrganizationCode = "";
+    String SendMoneyID = "";
+    String PaymentType = "";
+    String PayPartial = "";
+    String BankCode = "";
+    String ChequeNumber = "";
+    String ChequeBankBranch = "";
+    String ChequeDate = "";
+    String CreditCardNumber = "";
+    String CreditCardApproveCode = "";
+    String CreditEmployeeLevelPath = "";
+    String TripID = "";
+    String Status = "";
+    String RefNo = "";
+    String PayPeriod = "";
+    String PayDate = "";
+    String PAYAMT = "";
+    String CashCode = "";
+    String EmpID = "";
+    String TeamCode = "";
+    String receiptkind = "";
+    String Kind = "";
+    String BookNo = "";
+    String ReceiptNo = "";
+    String CreateDate = "";
+    String CreateBy = "";
+    String LastUpdateDate = "";
+    String LastUpdateBy = "";
+    String SyncedDate = "";
+
+    public String getReceiptCode() {
+        return ReceiptCode;
+    }
+
+    public void setReceiptCode(String receiptCode) {
+        ReceiptCode = receiptCode;
+    }
+
+
+    public String getCONTNO() {
+        return CONTNO;
+    }
+
+    public void setCONTNO(String CONTNO) {
+        this.CONTNO = CONTNO;
+    }
+
+    public String getCustomerName() {
+        return CustomerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        CustomerName = customerName;
+    }
+
+    public String getIDCard() {
+        return IDCard;
+    }
+
+    public void setIDCard(String IDCard) {
+        this.IDCard = IDCard;
+    }
+
+    public String getAddressInstall() {
+        return AddressInstall;
+    }
+
+    public void setAddressInstall(String addressInstall) {
+        AddressInstall = addressInstall;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public String getMODEL() {
+        return MODEL;
+    }
+
+    public void setMODEL(String MODEL) {
+        this.MODEL = MODEL;
+    }
+
+    public String getProductSerialNumber() {
+        return ProductSerialNumber;
+    }
+
+    public void setProductSerialNumber(String productSerialNumber) {
+        ProductSerialNumber = productSerialNumber;
+    }
+
+    public String getMaxPaymentPeriodNumber() {
+        return MaxPaymentPeriodNumber;
+    }
+
+    public void setMaxPaymentPeriodNumber(String maxPaymentPeriodNumber) {
+        MaxPaymentPeriodNumber = maxPaymentPeriodNumber;
+    }
+
+    public String getTotalPayment() {
+        return TotalPayment;
+    }
+
+    public void setTotalPayment(String totalPayment) {
+        TotalPayment = totalPayment;
+    }
+
+    public String getTotalPaymentText() {
+        return TotalPaymentText;
+    }
+
+    public void setTotalPaymentText(String totalPaymentText) {
+        TotalPaymentText = totalPaymentText;
+    }
+
+    public String getPeriodTotal() {
+        return PeriodTotal;
+    }
+
+    public void setPeriodTotal(String periodTotal) {
+        PeriodTotal = periodTotal;
+    }
+
+    public String getPeriodTotalPrice() {
+        return PeriodTotalPrice;
+    }
+
+    public void setPeriodTotalPrice(String periodTotalPrice) {
+        PeriodTotalPrice = periodTotalPrice;
+    }
+
+    public String getEFFDATE() {
+        return EFFDATE;
+    }
+
+    public void setEFFDATE(String EFFDATE) {
+        this.EFFDATE = EFFDATE;
+    }
+
+    public String getDatePayment() {
+        return DatePayment;
+    }
+
+    public void setDatePayment(String datePayment) {
+        DatePayment = datePayment;
+    }
+
+    public String getChanelName() {
+        return ChanelName;
+    }
+
+    public void setChanelName(String chanelName) {
+        ChanelName = chanelName;
+    }
+
+    public String getTeamName() {
+        return TeamName;
+    }
+
+    public void setTeamName(String teamName) {
+        TeamName = teamName;
+    }
+
+    public String getSalePaymentPeriod_SalePaymentPeriodID() {
+        return SalePaymentPeriod_SalePaymentPeriodID;
+    }
+
+    public void setSalePaymentPeriod_SalePaymentPeriodID(String salePaymentPeriod_SalePaymentPeriodID) {
+        SalePaymentPeriod_SalePaymentPeriodID = salePaymentPeriod_SalePaymentPeriodID;
+    }
+
+    public String getSalePaymentPeriod_RefNo() {
+        return SalePaymentPeriod_RefNo;
+    }
+
+    public void setSalePaymentPeriod_RefNo(String salePaymentPeriod_RefNo) {
+        SalePaymentPeriod_RefNo = salePaymentPeriod_RefNo;
+    }
+
+    public String getSalePaymentPeriod_PaymentPeriodNumber() {
+        return SalePaymentPeriod_PaymentPeriodNumber;
+    }
+
+    public void setSalePaymentPeriod_PaymentPeriodNumber(String salePaymentPeriod_PaymentPeriodNumber) {
+        SalePaymentPeriod_PaymentPeriodNumber = salePaymentPeriod_PaymentPeriodNumber;
+    }
+
+    public String getSalePaymentPeriod_PaymentAmount() {
+        return SalePaymentPeriod_PaymentAmount;
+    }
+
+    public void setSalePaymentPeriod_PaymentAmount(String salePaymentPeriod_PaymentAmount) {
+        SalePaymentPeriod_PaymentAmount = salePaymentPeriod_PaymentAmount;
+    }
+
+    public String getSalePaymentPeriod_Discount() {
+        return SalePaymentPeriod_Discount;
+    }
+
+    public void setSalePaymentPeriod_Discount(String salePaymentPeriod_Discount) {
+        SalePaymentPeriod_Discount = salePaymentPeriod_Discount;
+    }
+
+    public String getSalePaymentPeriod_NetAmount() {
+        return SalePaymentPeriod_NetAmount;
+    }
+
+    public void setSalePaymentPeriod_NetAmount(String salePaymentPeriod_NetAmount) {
+        SalePaymentPeriod_NetAmount = salePaymentPeriod_NetAmount;
+    }
+
+    public String getSalePaymentPeriod_PaymentComplete() {
+        return SalePaymentPeriod_PaymentComplete;
+    }
+
+    public void setSalePaymentPeriod_PaymentComplete(String salePaymentPeriod_PaymentComplete) {
+        SalePaymentPeriod_PaymentComplete = salePaymentPeriod_PaymentComplete;
+    }
+
+    public String getSalePaymentPeriod_TripID() {
+        return SalePaymentPeriod_TripID;
+    }
+
+    public void setSalePaymentPeriod_TripID(String salePaymentPeriod_TripID) {
+        SalePaymentPeriod_TripID = salePaymentPeriod_TripID;
+    }
+
+    public String getSalePaymentPeriod_CreateBy() {
+        return SalePaymentPeriod_CreateBy;
+    }
+
+    public void setSalePaymentPeriod_CreateBy(String salePaymentPeriod_CreateBy) {
+        SalePaymentPeriod_CreateBy = salePaymentPeriod_CreateBy;
+    }
+
+    public String getSalePaymentPeriod_LastUpdateBy() {
+        return SalePaymentPeriod_LastUpdateBy;
+    }
+
+    public void setSalePaymentPeriod_LastUpdateBy(String salePaymentPeriod_LastUpdateBy) {
+        SalePaymentPeriod_LastUpdateBy = salePaymentPeriod_LastUpdateBy;
+    }
+
+    public String getSalePaymentPeriod_CloseAccountDiscountAmount() {
+        return SalePaymentPeriod_CloseAccountDiscountAmount;
+    }
+
+    public void setSalePaymentPeriod_CloseAccountDiscountAmount(String salePaymentPeriod_CloseAccountDiscountAmount) {
+        SalePaymentPeriod_CloseAccountDiscountAmount = salePaymentPeriod_CloseAccountDiscountAmount;
+    }
+
+    public String getSalePaymentPeriod_CreateDate() {
+        return SalePaymentPeriod_CreateDate;
+    }
+
+    public void setSalePaymentPeriod_CreateDate(String salePaymentPeriod_CreateDate) {
+        SalePaymentPeriod_CreateDate = salePaymentPeriod_CreateDate;
+    }
+
+    public String getSalePaymentPeriod_PaymentDueDate() {
+        return SalePaymentPeriod_PaymentDueDate;
+    }
+
+    public void setSalePaymentPeriod_PaymentDueDate(String salePaymentPeriod_PaymentDueDate) {
+        SalePaymentPeriod_PaymentDueDate = salePaymentPeriod_PaymentDueDate;
+    }
+
+    public String getSalePaymentPeriod_PaymentAppointmentDate() {
+        return SalePaymentPeriod_PaymentAppointmentDate;
+    }
+
+    public void setSalePaymentPeriod_PaymentAppointmentDate(String salePaymentPeriod_PaymentAppointmentDate) {
+        SalePaymentPeriod_PaymentAppointmentDate = salePaymentPeriod_PaymentAppointmentDate;
+    }
+
+    public String getSalePaymentPeriod_LastUpdateDate() {
+        return SalePaymentPeriod_LastUpdateDate;
+    }
+
+    public void setSalePaymentPeriod_LastUpdateDate(String salePaymentPeriod_LastUpdateDate) {
+        SalePaymentPeriod_LastUpdateDate = salePaymentPeriod_LastUpdateDate;
+    }
+
+    public String getSalePaymentPeriod_SyncedDate() {
+        return SalePaymentPeriod_SyncedDate;
+    }
+
+    public void setSalePaymentPeriod_SyncedDate(String salePaymentPeriod_SyncedDate) {
+        SalePaymentPeriod_SyncedDate = salePaymentPeriod_SyncedDate;
+    }
+
+    public String getDatePayment1() {
+        return DatePayment1;
+    }
+
+    public void setDatePayment1(String datePayment1) {
+        DatePayment1 = datePayment1;
+    }
+
+    public String getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(String paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public String getOrganizationCode() {
+        return OrganizationCode;
+    }
+
+    public void setOrganizationCode(String organizationCode) {
+        OrganizationCode = organizationCode;
+    }
+
+    public String getSendMoneyID() {
+        return SendMoneyID;
+    }
+
+    public void setSendMoneyID(String sendMoneyID) {
+        SendMoneyID = sendMoneyID;
+    }
+
+    public String getPaymentType() {
+        return PaymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        PaymentType = paymentType;
+    }
+
+    public String getPayPartial() {
+        return PayPartial;
+    }
+
+    public void setPayPartial(String payPartial) {
+        PayPartial = payPartial;
+    }
+
+    public String getBankCode() {
+        return BankCode;
+    }
+
+    public void setBankCode(String bankCode) {
+        BankCode = bankCode;
+    }
+
+    public String getChequeNumber() {
+        return ChequeNumber;
+    }
+
+    public void setChequeNumber(String chequeNumber) {
+        ChequeNumber = chequeNumber;
+    }
+
+    public String getChequeBankBranch() {
+        return ChequeBankBranch;
+    }
+
+    public void setChequeBankBranch(String chequeBankBranch) {
+        ChequeBankBranch = chequeBankBranch;
+    }
+
+    public String getChequeDate() {
+        return ChequeDate;
+    }
+
+    public void setChequeDate(String chequeDate) {
+        ChequeDate = chequeDate;
+    }
+
+    public String getCreditCardNumber() {
+        return CreditCardNumber;
+    }
+
+    public void setCreditCardNumber(String creditCardNumber) {
+        CreditCardNumber = creditCardNumber;
+    }
+
+    public String getCreditCardApproveCode() {
+        return CreditCardApproveCode;
+    }
+
+    public void setCreditCardApproveCode(String creditCardApproveCode) {
+        CreditCardApproveCode = creditCardApproveCode;
+    }
+
+    public String getCreditEmployeeLevelPath() {
+        return CreditEmployeeLevelPath;
+    }
+
+    public void setCreditEmployeeLevelPath(String creditEmployeeLevelPath) {
+        CreditEmployeeLevelPath = creditEmployeeLevelPath;
+    }
+
+    public String getTripID() {
+        return TripID;
+    }
+
+    public void setTripID(String tripID) {
+        TripID = tripID;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+
+    public String getRefNo() {
+        return RefNo;
+    }
+
+    public void setRefNo(String refNo) {
+        RefNo = refNo;
+    }
+
+    public String getPayPeriod() {
+        return PayPeriod;
+    }
+
+    public void setPayPeriod(String payPeriod) {
+        PayPeriod = payPeriod;
+    }
+
+    public String getPayDate() {
+        return PayDate;
+    }
+
+    public void setPayDate(String payDate) {
+        PayDate = payDate;
+    }
+
+    public String getPAYAMT() {
+        return PAYAMT;
+    }
+
+    public void setPAYAMT(String PAYAMT) {
+        this.PAYAMT = PAYAMT;
+    }
+
+    public String getCashCode() {
+        return CashCode;
+    }
+
+    public void setCashCode(String cashCode) {
+        CashCode = cashCode;
+    }
+
+    public String getEmpID() {
+        return EmpID;
+    }
+
+    public void setEmpID(String empID) {
+        EmpID = empID;
+    }
+
+    public String getTeamCode() {
+        return TeamCode;
+    }
+
+    public void setTeamCode(String teamCode) {
+        TeamCode = teamCode;
+    }
+
+    public String getReceiptkind() {
+        return receiptkind;
+    }
+
+    public void setReceiptkind(String receiptkind) {
+        this.receiptkind = receiptkind;
+    }
+
+    public String getKind() {
+        return Kind;
+    }
+
+    public void setKind(String kind) {
+        Kind = kind;
+    }
+
+    public String getBookNo() {
+        return BookNo;
+    }
+
+    public void setBookNo(String bookNo) {
+        BookNo = bookNo;
+    }
+
+    public String getReceiptNo() {
+        return ReceiptNo;
+    }
+
+    public void setReceiptNo(String receiptNo) {
+        ReceiptNo = receiptNo;
+    }
+
+    public String getCreateDate() {
+        return CreateDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        CreateDate = createDate;
+    }
+
+    public String getCreateBy() {
+        return CreateBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        CreateBy = createBy;
+    }
+
+    public String getLastUpdateDate() {
+        return LastUpdateDate;
+    }
+
+    public void setLastUpdateDate(String lastUpdateDate) {
+        LastUpdateDate = lastUpdateDate;
+    }
+
+    public String getLastUpdateBy() {
+        return LastUpdateBy;
+    }
+
+    public void setLastUpdateBy(String lastUpdateBy) {
+        LastUpdateBy = lastUpdateBy;
+    }
+
+    public String getSyncedDate() {
+        return SyncedDate;
+    }
+
+    public void setSyncedDate(String syncedDate) {
+        SyncedDate = syncedDate;
+    }
+}
